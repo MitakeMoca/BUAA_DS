d501a74945a4914ef5468f14bf23691e4f6c8d15
Moca114514 <1043530132@qq.com>
2025-05-06 00:06:14 +0800

    feat: udpate func solution

--- 作业讲解/第四次作业（栈和队）/6-函数调用关系.c
+++ 作业讲解/第四次作业（栈和队）/6-函数调用关系.c
@@ -9,36 +9,36 @@ int readInt() {
 }
 
 // 记录函数名
-char *name[1005], total;
+char *func[1005], total;
 // 每个函数调用了多少子函数，编号分别是多少
-int len[1005], id[1005][50];
+int len[1005], calls[1005][50];
 // 记录函数 i 是否调用过函数 j，避免重复输出
 int vis[1005][1005];
 int stk[1005], top = -1;
 
 int find(char *s) {
     for(int i = 0; i < total; i++) 
-        if(strcmp(name[i], s) == 0) 
+        if(strcmp(func[i], s) == 0) 
             return i;
     
-    name[total] = (char *)malloc(strlen(s) + 1);
-    strcpy(name[total], s);
+    func[total] = (char *)malloc(strlen(s) + 1);
+    strcpy(func[total], s);
     return total++;
 }
 
-char func[1005];
+char str[1005];
 int main() {
     int op;
     while(1) {
         op = readInt();
         if(op == 8) {
-            scanf("%s", func);
-            int newId = find(func);
+            scanf("%s", str);
+            int newId = find(str);
             if(top != -1) {
-                int topId = find(name[stk[top]]);
+                int topId = find(func[stk[top]]);
                 if(!vis[topId][newId]) {
                     vis[topId][newId] = 1;
-                    id[topId][len[topId]++] = newId;
+                    calls[topId][len[topId]++] = newId;
                 }
             }
             stk[++top] = newId;
@@ -51,10 +51,10 @@ int main() {
     // 输出阶段
     for(int i = 0; i < total; i++) {
         if(len[i] == 0) continue;
-        printf("%s:", name[i]);
+        printf("%s:", func[i]);
         for(int j = 0; j < len[i]; j++) {
             if(j) printf(",");
-            printf("%s", name[id[i][j]]);
+            printf("%s", func[calls[i][j]]);
         }
         printf("\n");
     }
